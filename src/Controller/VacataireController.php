3c9f9fb7bf851097d1944a677e5a6197c52e6894
tg853309 <tgou58@gmail.com>
2026-06-25 14:57:40 +0200

    Formulaire pour créer un vacataire

--- src/Controller/VacataireController.php
+++ src/Controller/VacataireController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Vacataire;
+use App\Form\VacataireTypeForm;
 use App\Repository\VacataireRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,8 +19,7 @@ final class VacataireController extends AbstractController
         VacataireRepository $repository,
         PaginatorInterface $paginator,
         Request $request
-    ): Response
-    {
+    ): Response {
         $vacataires = $paginator->paginate(
             $repository->findAll(),
             $request->query->getInt('page', 1),
@@ -26,6 +28,30 @@ final class VacataireController extends AbstractController
 
         return $this->render('pages/vacataire/index.html.twig', [
             'vacataires' => $vacataires,
+        ]);
+    }
+
+    #[Route('/vacataire/nouveau', name: 'vacataire_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $vacataire = new Vacataire();
+        $form = $this->createForm(VacataireTypeForm::class, $vacataire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($vacataire);
+            $em->flush();
+
+            $this->addFlash(
+                    'success',
+                'Vos changements ont été enregistrés !'
+            );
+
+            return $this->redirectToRoute('app_vacataire');
+        }
+
+        return $this->render('pages/vacataire/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
